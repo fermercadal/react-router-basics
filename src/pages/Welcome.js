4e16e782c9705d0808a550427297c56162c288b5
Fernando Mercadal <fermercadal@outlook.com>
2021-11-01 10:36:37 -0300

    Adding a nested route

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,17 @@
+import { Route, NavLink } from "react-router-dom";
+
 const Welcome = () => {
-  return <h1>The Welcome Page</h1>;
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+
+      <NavLink to="/welcome/new-user">Say hello</NavLink>
+
+      <Route path="/welcome/new-user">
+        <p>Welcome, new user</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
